05bc83d4ddf9bffe4b9cdc2f4287e69ddb905e90
pupil <pupil@ivkhk.ee>
2020-09-21 10:43:07 +0300

    приёмы работы со строками

--- src/jptvr19perevertish/JPTVR19Perevertish.java
+++ src/jptvr19perevertish/JPTVR19Perevertish.java
@@ -15,7 +15,9 @@ public class JPTVR19Perevertish {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        App app = new App ();
+        app.run();
+                
     }
     
 }
